6910d3a432ebe3b1ab7c64e9e7ee930043132ca5
xiasx@txtws.com <x12345678>
2019-07-01 11:17:15 +0800

    WS-2019.1.3 <Dell@DESKTOP-SGIU9UV Create baseRefactoring.xml, markdown.xml, Vue Single File Component.vue, ide.general.xml, git.xml, project.default.xml, Default.xml, find.xml, darculaas.icls, grey_orange.xml, filetypes.xml, projectView.xml, watcherDefaultTasks.xml, colors.scheme.xml, Vue File.vue, Default.xml, laf.xml, DefaultCopy.icls, path.macros.xml, editor.codeinsight.xml, grey_orange.icls, grey_orange - 副本.icls, editor.xml, usageView.xml, Default2.icls, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="component-card">
+    #[[$END$]]#
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '${KEBAB_CASE_NAME}',
+    beforecreate : function() {},
+    props: {},
+    data: function() {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    mounted: function() {},
+    methods: {}
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .component-card{
+    width: 100%;
+    height: 100%;
+  }
+</style>
